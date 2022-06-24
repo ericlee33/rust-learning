c0c088b4c692ad526ed9e0f76e437b5baff62fe2
Eric Lee <ericfly33@gmail.com>
2022-06-25 01:40:51 +0800

    module-study

---
+++ crates/module-study/src/lib.rs
@@ -0,0 +1,25 @@
+mod back_to_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_to_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("{}", meal.toast);
+    meal.seasonal_fruit = String::from("blueberries");
+}
+
+// fn main() {
+// }
