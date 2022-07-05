1097e44092146139955c939d2f3679dce9d660ca
Eric Lee <ericfly33@gmail.com>
2022-07-06 01:28:59 +0800

    add main

--- crates/test-study/src/lib.rs
+++ crates/test-study/src/lib.rs
@@ -1,19 +1,19 @@
-#[cfg(test)]
-mod tests {
-    // #[test]
-    // #[should_panic]
-    // fn it_works() {
-    //     let result = 2 + 2;
-    //     panic!("sorry");
-    //     assert_eq!(result, 4);
-    // }
+// #[cfg(test)]
+// mod tests {
+//     // #[test]
+//     // #[should_panic]
+//     // fn it_works() {
+//     //     let result = 2 + 2;
+//     //     panic!("sorry");
+//     //     assert_eq!(result, 4);
+//     // }
 
-    #[test]
-    fn works_error() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Err(String::from("wrong"))
-        } else {
-            Ok(())
-        }
-    }
-}
+//     #[test]
+//     fn works_error() -> Result<(), String> {
+//         if 2 + 2 == 4 {
+//             Err(String::from("wrong"))
+//         } else {
+//             Ok(())
+//         }
+//     }
+// }
